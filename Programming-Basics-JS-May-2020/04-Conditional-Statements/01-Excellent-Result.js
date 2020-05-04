9e64c71ba777709b072c59ede86a5057c49b2d91
Wiched <wiched@users.noreply.github.com>
2020-05-04 15:55:33 +0300

    created 01-Excellent-Result.js

---
+++ Programming-Basics-JS-May-2020/04-Conditional-Statements/01-Excellent-Result.js
@@ -0,0 +1,8 @@
+function excellentResult(grade) {
+  grade >= 5.5 ? console.log('Excellent!') : null;
+}
+
+excellentResult('6');
+excellentResult('5');
+excellentResult('5.50');
+excellentResult('5.49');
