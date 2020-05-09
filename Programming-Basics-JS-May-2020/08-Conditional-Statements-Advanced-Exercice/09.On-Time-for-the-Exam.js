f0985dde69a9dfd8a6ad438e02f147cd642c013c
Wiched <wiched@users.noreply.github.com>
2020-05-09 13:45:55 +0300

    Update 09.On-Time-for-the-Exam.js

--- Programming-Basics-JS-May-2020/08-Conditional-Statements-Advanced-Exercice/09.On-Time-for-the-Exam.js
+++ Programming-Basics-JS-May-2020/08-Conditional-Statements-Advanced-Exercice/09.On-Time-for-the-Exam.js
@@ -51,12 +51,12 @@ function onTimeForTheExam(examHour, examMinute, hour, minute) {
   }
 }
 
-// onTimeForTheExam('9', '30', '9', '50');
-// onTimeForTheExam('9', '00', '8', '30');
-// onTimeForTheExam('16', '00', '15', '00');
-// onTimeForTheExam('9', '00', '10', '30');
-// onTimeForTheExam('14', '00', '13', '55');
-// onTimeForTheExam('11', '30', '8', '12');
-// onTimeForTheExam('10', '00', '10', '00');
-// onTimeForTheExam('11', '30', '10', '55');
+onTimeForTheExam('9', '30', '9', '50');
+onTimeForTheExam('9', '00', '8', '30');
+onTimeForTheExam('16', '00', '15', '00');
+onTimeForTheExam('9', '00', '10', '30');
+onTimeForTheExam('14', '00', '13', '55');
+onTimeForTheExam('11', '30', '8', '12');
+onTimeForTheExam('10', '00', '10', '00');
+onTimeForTheExam('11', '30', '10', '55');
 onTimeForTheExam('11', '30', '12', '29');
