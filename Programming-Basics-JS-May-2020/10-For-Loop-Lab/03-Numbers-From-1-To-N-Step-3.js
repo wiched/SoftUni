fb984a208c56cd3ad41486322ab6c11b1d37622a
Wiched <wiched@users.noreply.github.com>
2020-05-10 17:28:18 +0300

    Create 03-Numbers-From-1-To-N-Step-3.js

---
+++ Programming-Basics-JS-May-2020/10-For-Loop-Lab/03-Numbers-From-1-To-N-Step-3.js
@@ -0,0 +1,11 @@
+function from1ToNStep3(num) {
+  num = +num;
+
+  for (let i = 1; i <= num; i += 3) {
+    console.log(i);
+  }
+}
+
+// from1ToNStep3('10');
+// from1ToNStep3('7');
+from1ToNStep3('15');
