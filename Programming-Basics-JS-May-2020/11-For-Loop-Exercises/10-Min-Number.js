45cd6ed69cba80f4e75b6005cfaa534d2ca4ee4a
Wiched <wiched@users.noreply.github.com>
2020-05-13 19:31:46 +0300

    Update 10-Min-Number.js

--- Programming-Basics-JS-May-2020/11-For-Loop-Exercises/10-Min-Number.js
+++ Programming-Basics-JS-May-2020/11-For-Loop-Exercises/10-Min-Number.js
@@ -1,6 +1,25 @@
+// Without loop
 function minNumber(params) {
   const n = params.shift();
   const lowest = Math.min(...params);
+  console.log(lowest);
+}
+
+minNumber(['2', '100', '99']);
+minNumber(['3', '-10', '20', '-30']);
+minNumber(['4', '45', '-20', '7', '99']);
+minNumber(['1', '999']);
+minNumber(['2', '-1', '-2']);
+
+// With loop
+function minNumber(params) {
+  const n = params.shift();
+  let lowest = Number.POSITIVE_INFINITY;
+  for (let i = 0; i < n; i++) {
+    if (params[i] < lowest) {
+      lowest = params[i];
+    }
+  }
   console.log(lowest);
 }
 
