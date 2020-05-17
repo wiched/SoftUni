9b6a30d5e66c1031721105617e730997c4196a52
Wiched <wiched@users.noreply.github.com>
2020-05-17 13:18:45 +0300

    Update 04-Walking.js

--- Programming-Basics-JS-May-2020/14-While-Loop-Exercises/04-Walking.js
+++ Programming-Basics-JS-May-2020/14-While-Loop-Exercises/04-Walking.js
@@ -2,8 +2,8 @@ function walking(params) {
   let steps = 0;
 
   while (steps < 10000) {
-    let currentSteps = Number(params.shift());
-    if (!isNaN(currentSteps)) {
+    let currentSteps = params.shift();
+    if (currentSteps != 'Going home') {
       steps += Number(currentSteps);
     } else {
       currentSteps = Number(params.shift());
@@ -13,16 +13,16 @@ function walking(params) {
   }
 
   if (steps >= 10000) {
-    const diff = steps - 10000;
+    const diff = +steps - 10000;
     console.log('Goal reached! Good job!');
     console.log(`${diff} steps over the goal!`);
   } else {
-    const diff = 10000 - steps;
+    const diff = 10000 - +steps;
     console.log(`${diff} more steps to reach goal.`);
   }
 }
 
 walking(['1000', '1500', '2000', '6500']);
-// walking(['1500', '3000', '250', '1548', '2000', 'Going home', '2000']);
+walking(['1500', '3000', '250', '1548', '2000', 'Going home', '2000']);
 walking(['1500', '300', '2500', '3000', 'Going home', '200']);
-// walking(['125', '250', '4000', '30', '2678', '4682']);
+walking(['125', '250', '4000', '30', '2678', '4682']);
