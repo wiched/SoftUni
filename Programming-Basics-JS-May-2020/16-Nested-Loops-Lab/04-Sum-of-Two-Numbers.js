8d996ab170291960a969b7bc8ca410b9b1a04507
Wiched <wiched@users.noreply.github.com>
2020-05-19 14:30:43 +0300

    Added homework

---
+++ Programming-Basics-JS-May-2020/16-Nested-Loops-Lab/04-Sum-of-Two-Numbers.js
@@ -0,0 +1,6 @@
+function sumOfTwoNumbers() { }
+
+sumOfTwoNumbers(['1', '10', '5']);
+// sumOfTwoNumbers(['23', '24', '20']);
+// sumOfTwoNumbers(['88', '888', '1000']);
+// sumOfTwoNumbers(['88', '888', '2000']);
