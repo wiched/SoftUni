bd54e90072efc2bb12cb004e72dfab1803313fdb
Wiched <wiched@users.noreply.github.com>
2020-05-19 18:54:03 +0300

    Update 05-Tournament-of-Christmas.js

--- Programming-Basics-JS-May-2020/16-Nested-Loops-Lab/05-Tournament-of-Christmas.js
+++ Programming-Basics-JS-May-2020/16-Nested-Loops-Lab/05-Tournament-of-Christmas.js
@@ -1,4 +1,41 @@
-function tournamentOfChristmas(params) { }
+function tournamentOfChristmas(params) {
+  const days = params.shift();
+  let totalMoney = 0;
+  let daysWon = 0;
+  let daysLoosed = 0;
+
+  for (let i = 1; i <= days; i++) {
+    let game = params.shift();
+    let dailyMoney = 0;
+    let wins = 0;
+    let loses = 0;
+
+    while (game !== 'Finish') {
+      const result = params.shift();
+      if (result === 'win') {
+        dailyMoney += 20;
+        wins++;
+      } else {
+        loses++;
+      }
+      game = params.shift();
+    }
+    if (wins > loses) {
+      dailyMoney *= 1.1;
+      daysWon++;
+    } else {
+      daysLoosed++;
+    }
+    totalMoney += dailyMoney;
+  }
+
+  if (daysWon > daysLoosed) {
+    totalMoney *= 1.2;
+    console.log(`You won the tournament! Total raised money: ${totalMoney.toFixed(2)}`);
+  } else {
+    console.log(`You lost the tournament! Total raised money: ${totalMoney.toFixed(2)}`);
+  }
+}
 
 tournamentOfChristmas([
   '2',
