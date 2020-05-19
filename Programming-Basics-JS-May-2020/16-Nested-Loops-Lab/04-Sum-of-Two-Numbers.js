385179b620cea483e93eaac33fa5dd1f2c804a2e
Wiched <wiched@users.noreply.github.com>
2020-05-19 17:21:45 +0300

    Created 05-Tournament-of-christmas.js

--- Programming-Basics-JS-May-2020/16-Nested-Loops-Lab/04-Sum-of-Two-Numbers.js
+++ Programming-Basics-JS-May-2020/16-Nested-Loops-Lab/04-Sum-of-Two-Numbers.js
@@ -35,5 +35,5 @@ function sumOfTwoNumbers(params) {
 
 sumOfTwoNumbers(['1', '10', '5']);
 sumOfTwoNumbers(['23', '24', '20']);
-// sumOfTwoNumbers(['88', '888', '1000']);
-// sumOfTwoNumbers(['88', '888', '2000']);
+sumOfTwoNumbers(['88', '888', '1000']);
+sumOfTwoNumbers(['88', '888', '2000']);
